3590eec38ae0c796ed71c35601f64f8d4317c373
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-09-16 17:16:21 -0400

    Cleanup digital ip flow Makefile

--- digital-ip.mk
+++ digital-ip.mk
@@ -2,14 +2,15 @@
 VERILATOR := verilator
 SLANG := slang
 YOSYS := yosys
+PEAKRDL := peakrdl
 
-# Linter: verilator
+# Linter options: verilator, slang
 LINTER := verilator
 
-# Simulator: verilator, iverilog
+# Simulator options: verilator
 SIMULATOR := verilator
 
-# Synthesis Tool: yosys, yosys-surelog, yosys-slang
+# Synthesis Tool options: yosys, yosys-surelog, yosys-slang
 SYNTH_TOOL := yosys
 
 VL_COMMON_ARGS :=
@@ -17,7 +18,7 @@ SLANG_COMMON_ARGS :=
 
 # UVM Specific Option
 UVM_PKG_ARGS := +incdir+ip/uvm/src ip/uvm/src/uvm_pkg.sv
-VL_UVM_ARGS := -DUVM_NO_DPI ip/flows/lint-waivers/uvm.vlt
+VL_UVM_ARGS := +define+UVM_NO_DPI
 
 # Verilator lint arguments
 VL_LINT_COMMON_ARGS := $(VL_COMMON_ARGS) -Wall
@@ -33,9 +34,11 @@ SLANG_VERIF_LINT_ARGS := $(SLANG_LINT_COMMON_ARGS)
 # Verilator simulation arguments
 VL_SIM_COMMON_ARGS := $(VL_COMMON_ARGS) -Wno-fatal
 ifeq ($(WAVE), vcd)
-VL_SIM_COMMON_ARGS += --trace --trace-structs
+VL_SIM_COMMON_ARGS += --trace-vcd --trace-structs
 else ifeq ($(WAVE), fst)
 VL_SIM_COMMON_ARGS += --trace-fst --trace-structs
+else ifeq ($(WAVE), saif)
+VL_SIM_COMMON_ARGS += --trace-saif --trace-structs
 endif
 ifeq ($(ASSERT), 0)
 VL_SIM_COMMON_ARGS += --no-assert
@@ -81,7 +84,7 @@ lint-tb:
 
 .PHONY: lint-verif
 lint-verif:
-	$(VERILATOR) --lint-only $(VL_VERIF_LINT_ARGS) $(RTL_FLIST_ARG) $(VERIF_FLIST_ARG) --top $(VERIF_TOP_NAME)
+	$(VERILATOR) --lint-only $(VL_VERIF_LINT_ARGS) $(UVM_PKG_ARGS) $(RTL_FLIST_ARG) $(VERIF_FLIST_ARG) --top $(VERIF_TOP_NAME)
 
 else ifeq ($(LINTER), slang)
 
@@ -96,7 +99,7 @@ lint-tb:
 
 .PHONY: lint-verif
 lint-verif:
-	$(SLANG) $(SLANG_VERIF_LINT_ARGS) $(RTL_FLIST_ARG) $(VERIF_FLIST_ARG) --top $(VERIF_TOP_NAME)
+	$(SLANG) $(SLANG_VERIF_LINT_ARGS) $(UVM_PKG_ARGS) $(RTL_FLIST_ARG) $(VERIF_FLIST_ARG) --top $(VERIF_TOP_NAME)
 
 endif
 
@@ -110,9 +113,10 @@ cdc:
 rdc:
 	echo "Reset domain crossing checking not available"
 
+ifeq ($(SIMULATOR), verilator)
+
 # Simple SystemVerilog Testbench Simulation
 # Build simulation executable
-ifeq ($(SIMULATOR), verilator)
 sim/$(TB_NAME)_obj_dir/V$(TB_NAME):
 	mkdir -p sim
 	$(VERILATOR) --cc --exe --main $(VL_TB_SIM_ARGS) --top $(TB_NAME) $(RTL_FLIST_ARG) $(TB_FILE) -Mdir sim/$(TB_NAME)_obj_dir
@@ -146,17 +150,6 @@ run-test: sim/verif_obj_dir/V$(VERIF_TOP_NAME)
 	mkdir -p sim/$(TEST)
 	cd sim ; ./verif_obj_dir/V$(VERIF_TOP_NAME) +UVM_TESTNAME=$(TEST) $(SIM_ARGS)
 
-else ifeq ($(SIMULATOR), iverilog)
-sim/icarus_$(TB_NAME): rtl_filelist.f
-	iverilog -g2012 -Y.sv -f rtl_filelist.f $(TB_FILE) -s $(TB_NAME) -o sim/icarus_$(TB_NAME)
-
-.PHONY: build-tb-sim
-build-tb-sim: sim/icarus_$(TB_NAME)
-
-.PHONY: run-tb-sim
-run-tb-sim: sim/icarus_$(TB_NAME)
-	cd sim ; ./icarus_$(TB_NAME) $(SIM_ARGS)
-
 endif
 
 # Formal property verification
@@ -166,23 +159,17 @@ formal:
 
 # CSR
 .PHONY: csr
-csr: csr-rtl csr-ral csr-doc
+csr: csr-rtl csr-ral
 
 # Generate CSR block RTL
 .PHONY: csr-rtl
 csr-rtl:
-	peakrdl regblock csr/$(CSR_BLOCK_NAME).rdl -o rtl/ --cpuif apb4-flat --peakrdl-cfg ip/flows/peakrdl/peakrdl.toml
+	$(PEAKRDL) regblock csr/$(CSR_BLOCK_NAME).rdl -o rtl/ --cpuif apb4-flat --peakrdl-cfg ip/flows/peakrdl/peakrdl.toml
 
 # Generate CSR UVM RAL model
 .PHONY: csr-ral
 csr-ral:
-	peakrdl uvm csr/$(CSR_BLOCK_NAME).rdl -o verif/$(CSR_BLOCK_NAME)_ral_pkg.sv --peakrdl-cfg ip/flows/peakrdl/peakrdl.toml
-
-# Generate CSR reStructuredText Documentation
-.PHONY: csr-doc
-csr-doc:
-	mkdir -p sim
-	peakrdl html csr/$(CSR_BLOCK_NAME).rdl -o doc/ --peakrdl-cfg ip/flows/peakrdl/peakrdl.toml
+	$(PEAKRDL) uvm csr/$(CSR_BLOCK_NAME).rdl -o verif/$(CSR_BLOCK_NAME)_ral_pkg.sv --peakrdl-cfg ip/flows/peakrdl/peakrdl.toml
 
 # Synthesis
 ifeq ($(SYNTH_TOOL), yosys)
@@ -190,63 +177,23 @@ ifeq ($(SYNTH_TOOL), yosys)
 .PHONY: synth
 synth:
 	mkdir -p netlist
-	yosys -p "read_verilog_file_list $(RTL_FLIST_ARG); synth -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME)_generic.netlist.v"
-
-.PHONY: synth-asic
-synth-asic:
-	mkdir -p netlist
-	yosys -p "read_verilog_file_list $(RTL_FLIST_ARG); read_liberty -ignore_miss_func $(LIBERTY_FILE); synth -top $(RTL_TOP_NAME) -flatten; dfflibmap -liberty $(LIBERTY_FILE); abc -liberty $(LIBERTY_FILE); clean; stat; write_verilog netlist/$(RTL_TOP_NAME)_asic.netlist.v"
-
-.PHONY: synth-fpga
-synth-fpga:
-	mkdir -p netlist
-	yosys -p "read_verilog_file_list $(RTL_FLIST_ARG); synth_$(FPGA_VENDOR) -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME)_fpga.netlist.v"
+	$(YOSYS) -p "read_verilog_file_list $(RTL_FLIST_ARG); synth -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME).netlist.v"
 
 else ifeq ($(SYNTH_TOOL), yosys-surelog)
 
 .PHONY: synth
 synth: rtl_filelist.f
 	mkdir -p netlist
-	yosys -m systemverilog -p "read_systemverilog -f rtl_filelist.f; synth -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME)_generic.netlist.v"
-
-.PHONY: synth-asic
-synth-asic: rtl_filelist.f
-	mkdir -p netlist
-	yosys -m systemverilog -p "read_systemverilog -f rtl_filelist.f; read_liberty -ignore_miss_func $(LIBERTY_FILE); synth -top $(RTL_TOP_NAME) -flatten; dfflibmap -liberty $(LIBERTY_FILE); abc -liberty $(LIBERTY_FILE); clean; stat; write_verilog netlist/$(RTL_TOP_NAME)_asic.netlist.v"
-
-.PHONY: synth-fpga
-synth-fpga: rtl_filelist.f
-	mkdir -p netlist
-	yosys -m systemverilog -p "read_systemverilog -f rtl_filelist.f; synth_$(FPGA_VENDOR) -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME)_fpga.netlist.v"
+	$(YOSYS) -m systemverilog -p "read_systemverilog -f rtl_filelist.f; synth -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME).netlist.v"
 
 else ifeq ($(SYNTH_TOOL), yosys-slang)
 
 .PHONY: synth
 synth:
 	mkdir -p netlist
-	yosys -m slang -p "read_slang -Weverything --top $(RTL_TOP_NAME) +define+SYNTHESIS $(RTL_FLIST_ARG); synth -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME)_generic.netlist.v"
-
-.PHONY: synth-asic
-synth-asic:
-	mkdir -p netlist
-	yosys -m slang -p "read_slang -Weverything --top $(RTL_TOP_NAME) +define+SYNTHESIS $(RTL_FLIST_ARG); read_liberty -ignore_miss_func $(LIBERTY_FILE); synth -top $(RTL_TOP_NAME) -flatten; dfflibmap -liberty $(LIBERTY_FILE); abc -liberty $(LIBERTY_FILE); clean; stat; write_verilog netlist/$(RTL_TOP_NAME)_asic.netlist.v"
-
-.PHONY: synth-fpga
-synth-fpga:
-	mkdir -p netlist
-	yosys -m slang -p "read_slang -Weverything --top $(RTL_TOP_NAME) +define+SYNTHESIS $(RTL_FLIST_ARG); synth_$(FPGA_VENDOR) -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME)_fpga.netlist.v"
+	$(YOSYS) -m slang -p "read_slang -Weverything --top $(RTL_TOP_NAME) +define+SYNTHESIS $(RTL_FLIST_ARG); synth -top $(RTL_TOP_NAME) -flatten; write_verilog netlist/$(RTL_TOP_NAME).netlist.v"
 
 endif
-
-# Static Timing Analysis
-.PHONY: sta
-sta:
-	echo "STA not available"
-
-# Logic Equivalant Check
-.PHONY: lec
-lec:
-	echo "LEC not available"
 
 # Clean
 .PHONY: clean
